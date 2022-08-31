12e8addf479c4520c86ab6cb232373eec5826072
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-08-31 21:22:26 +0700

    8 revisi praktikum2.dart

--- 08_Advance_Function_Async-Await/praktikum/praktikum2.dart
+++ 08_Advance_Function_Async-Await/praktikum/praktikum2.dart
@@ -1,16 +1,17 @@
 void main(List<String> args) {
-  // var data = {'23': 1};
-  var dataList = [
+  List<List<int>> dataList = [
     [1, 2],
     [3, 4],
     [5, 6],
-    [7, 8]
+    [7, 8],
+    [9, 10]
   ];
   print('List Data: $dataList');
 
-  var dataMap = {};
-  dataMap.addAll({0: dataList});
+  Map<int, int> dataMap = {};
+  dataList.forEach((e) => dataMap.addAll({e[0]: e[1]}));
   print('Map Data: $dataMap');
+  dataMap.forEach((key, value) => print('$key: $value'));
 }
 
 // Paste this command on your terminal to see the result / F5
